a10390911b45edd09694dae4ccc8f5b1cd8267e5
hwi-middle <gnlwnd5084@gmail.com>
2021-05-05 19:39:09 +0900

    최대 점프 수 제한 구현

--- 002. Platformer Demo/Assets/Player.cs
+++ 002. Platformer Demo/Assets/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
     public float speed;
     public float maxSpeed;
     public float jumpPower;
+    public int maxJumpCount = 1;
+    private int curJumpCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +20,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonUp("Horizontal"))
+        if (Input.GetButtonUp("Horizontal"))
         {
             rbd.velocity = new Vector2(rbd.velocity.normalized.x * 0.5f, rbd.velocity.y);
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && curJumpCount < maxJumpCount)
         {
             rbd.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            curJumpCount++;
         }
     }
 
@@ -40,6 +44,18 @@ public class Player : MonoBehaviour
         else if (rbd.velocity.x < -1 * maxSpeed)
         {
             rbd.velocity = new Vector2(-1 * maxSpeed, rbd.velocity.y);
+        }
+        Debug.DrawRay(rbd.position, Vector3.down, new Color(1, 0, 0));
+
+        RaycastHit2D rayHit = Physics2D.Raycast(rbd.position, Vector3.down, 1f, LayerMask.GetMask("Ground"));
+
+        //·¹ÀÌ¿¡ ¹º°¡ ´ê¾Ò°í ³«ÇÏÁßÀÏ °æ¿ì
+        if (rayHit.collider != null && rbd.velocity.y < 0)
+        {
+            if (rayHit.distance < 0.5)
+            {
+                curJumpCount = 0;
+            }
         }
     }
 }
